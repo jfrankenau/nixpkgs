386ad64661970cf3e66ce2493b85c7a05ef0e656
Johannes Frankenau <johannes@frankenau.net>
2018-11-19 20:32:44 +0100

    WIP

--- pkgs/applications/window-managers/i3/lock.nix
+++ pkgs/applications/window-managers/i3/lock.nix
@@ -1,18 +1,18 @@
-{ fetchurl, stdenv, which, pkgconfig, libxcb, xcbutilkeysyms, xcbutilimage,
-  pam, libX11, libev, cairo, libxkbcommon, libxkbfile }:
+{ fetchurl, stdenv, autoreconfHook, pkgconfig, libxcb, xcbutilkeysyms, xcbutilimage,
+  pam, libX11, libev, cairo, libxkbcommon, libxkbfile, xcbutilxrm }:
 
 stdenv.mkDerivation rec {
   name = "i3lock-${version}";
-  version = "2.10";
+  version = "2.11.1";
 
   src = fetchurl {
     url = "https://i3wm.org/i3lock/${name}.tar.bz2";
-    sha256 = "1vn8828ih7mpdl58znfnzpdwdgwksq16rghm5qlppbbz66zk5sr9";
+    sha256 = "015dn534m7cxjvqdncpvaq9p8b2r4w5hp1yanbdy2abmhbcc7a7j";
   };
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ which libxcb xcbutilkeysyms xcbutilimage pam libX11
-    libev cairo libxkbcommon libxkbfile ];
+  nativeBuildInputs = [ autoreconfHook pkgconfig ];
+  buildInputs = [ libxcb xcbutilkeysyms xcbutilimage pam libX11
+    libev cairo libxkbcommon libxkbfile xcbutilxrm ];
 
   makeFlags = "all";
   installFlags = "PREFIX=\${out} SYSCONFDIR=\${out}/etc";
